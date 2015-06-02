4ea275f1f53a7e78e1edf949696c7e948f25c67c
robneu <rob@wpsitecare.com>
2015-06-02 02:10:13 -0500

    Removed GitHub Plugin updater tags
    Since this plugin is now hosted on WordPress.org, I don’t see any
    reason to keep these. I know at least one of them is supposed to remain
    compatible with .org updates, but I’d rather just avoid potential
    problems.

--- wp-featherlight.php
+++ wp-featherlight.php
@@ -10,9 +10,6 @@
  * License URI:  http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:  wp-featherlight
  * Domain Path:  /languages
- * Git URI:      https://github.com/wpsitecare/wp-featherlight
- * GitHub Plugin URI: https://github.com/wpsitecare/wp-featherlight
- * GitHub Branch: master
  */
 
 // Prevent direct access.
