77c4978b6a2256a2087b18287e1ddea1d2c65bb1
robneu <rob@wpsitecare.com>
2015-06-17 05:38:21 -0500

    Fixed some jshint issues
    This script isn’t really actively maintained, so eh…

--- js/src/vendor/jquery.detect_swipe.js
+++ js/src/vendor/jquery.detect_swipe.js
@@ -5,6 +5,7 @@
  * Based on touchwipe by Andreas Waltl, netCU Internetagentur (http://www.netcu.de)
  */
 (function($) {
+	'use strict';
 
 	$.detectSwipe = {
 		version: '2.1.1',
@@ -24,20 +25,21 @@
 	}
 
 	function onTouchMove(e) {
-		if ($.detectSwipe.preventDefault) { e.preventDefault(); }
+		if ($.detectSwipe.preventDefault) {
+			e.preventDefault();
+		}
 		if(isMoving) {
-			var x = e.touches[0].pageX;
-			var y = e.touches[0].pageY;
-			var dx = startX - x;
-			var dy = startY - y;
-			var dir;
-			if(Math.abs(dx) >= $.detectSwipe.threshold) {
-				dir = dx > 0 ? 'left' : 'right'
+			var x = e.touches[0].pageX,
+				y = e.touches[0].pageY,
+				dx = startX - x,
+				dy = startY - y,
+				dir;
+			if (Math.abs(dx) >= $.detectSwipe.threshold) {
+				dir = dx > 0 ? 'left' : 'right';
+			} else if (Math.abs(dy) >= $.detectSwipe.threshold) {
+				dir = dy > 0 ? 'down' : 'up';
 			}
-			else if(Math.abs(dy) >= $.detectSwipe.threshold) {
-				dir = dy > 0 ? 'down' : 'up'
-			}
-			if(dir) {
+			if (dir) {
 				onTouchEnd.call(this);
 				$(this).trigger('swipe', dir).trigger('swipe' + dir);
 			}
@@ -45,7 +47,7 @@
 	}
 
 	function onTouchStart(e) {
-		if (e.touches.length == 1) {
+		if (e.touches.length === 1) {
 			startX = e.touches[0].pageX;
 			startY = e.touches[0].pageY;
 			isMoving = true;
@@ -64,8 +66,8 @@
 
 	$.event.special.swipe = { setup: setup };
 
-	$.each(['left', 'up', 'down', 'right'], function () {
-		$.event.special['swipe' + this] = { setup: function(){
+	$.each(['left', 'up', 'down', 'right'], function() {
+		$.event.special['swipe' + this] = { setup: function() {
 			$(this).on('swipe', $.noop);
 		} };
 	});
