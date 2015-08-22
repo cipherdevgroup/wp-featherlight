fa837e5983394a99deaeb1821d41705cf1c9791a
robneu <rob@wpsitecare.com>
2015-08-21 22:34:29 -0500

    Updated admin metabox string

--- admin/templates/metabox-sidebar.php
+++ admin/templates/metabox-sidebar.php
@@ -12,7 +12,7 @@
 <p>
 	<label for="wp_featherlight_disable">
 		<input type="checkbox" name="wp_featherlight_disable" id="wp_featherlight_disable" value="yes"<?php checked( $checked, 'yes' ); ?> />
-		<?php echo esc_html( sprintf( _x( 'Disable Lightbox on This %s', '%s = post type singular name', 'wp-featherlight' ), $name ) ); ?>
+		<?php echo esc_html( sprintf( _x( 'Disable Lightbox on this %s', '%s = post type singular name', 'wp-featherlight' ), $name ) ); ?>
 	</label>
 </p>
 <?php wp_nonce_field( 'toggle_wp_featherlight', 'wp_featherlight_disable_nonce' ); ?>
