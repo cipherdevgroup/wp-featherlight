ccb1609e0a3165a895d7c7d5e7e41a27bb338e5f
robneu <rob@wpsitecare.com>
2016-11-10 21:06:59 -0500

    Cleared out existing master branch

--- gruntfile.js
+++
@@ -1,9 +0,0 @@
-/*jshint node:true */
-
-module.exports = function( grunt ) {
-	'use strict';
-	var config = require( 'sitecare-plugin-config' );
-	require( 'sitecare-grunt-config-loader' )( grunt, config ).init({
-		pkg: grunt.file.readJSON( 'package.json' )
-	});
-};
